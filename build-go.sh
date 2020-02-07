5c3082821729969db8c09da850aed64df981bdf8
alexanderz <a.zhulin@12storeez.com>
2020-02-07 12:10:11 +0300

    add payments

--- build-go.sh
+++ build-go.sh
@@ -2,3 +2,4 @@ protoc -I. --micro_out=. --go_out=plugins=grpc:. proto/mindbox.proto
 protoc -I. --micro_out=. --go_out=plugins=grpc:. proto/logistics.proto
 protoc -I. --micro_out=. --go_out=plugins=grpc:. proto/orders.proto
 protoc -I. --micro_out=. --go_out=plugins=grpc:. proto/meta.proto
+protoc -I. --micro_out=. --go_out=plugins=grpc:. proto/payments.proto
