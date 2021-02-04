6083c17323479508fefe90db75fe335db39a21c4
Dmitriy Zotov <ArvarohFX@gmail.com>
2021-02-04 12:06:55 +0300

    Update slack notifications proto (#45)
    Co-authored-by: d.zotov <d.zotov@12storeez.com>

--- go/notifications/slack/slack_grpc.pb.go
+++ go/notifications/slack/slack_grpc.pb.go
@@ -11,6 +11,7 @@ import (
 
 // This is a compile-time assertion to ensure that this generated file
 // is compatible with the grpc package it is being compiled against.
+// Requires gRPC-Go v1.32.0 or later.
 const _ = grpc.SupportPackageIsVersion7
 
 // SlackClient is the client API for Slack service.
@@ -60,7 +61,7 @@ type UnsafeSlackServer interface {
 }
 
 func RegisterSlackServer(s grpc.ServiceRegistrar, srv SlackServer) {
-	s.RegisterService(&_Slack_serviceDesc, srv)
+	s.RegisterService(&Slack_ServiceDesc, srv)
 }
 
 func _Slack_Send_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
@@ -81,7 +82,10 @@ func _Slack_Send_Handler(srv interface{}, ctx context.Context, dec func(interfac
 	return interceptor(ctx, in, info, handler)
 }
 
-var _Slack_serviceDesc = grpc.ServiceDesc{
+// Slack_ServiceDesc is the grpc.ServiceDesc for Slack service.
+// It's only intended for direct use with grpc.RegisterService,
+// and not to be introspected or modified (even as a copy)
+var Slack_ServiceDesc = grpc.ServiceDesc{
 	ServiceName: "notifications.Slack",
 	HandlerType: (*SlackServer)(nil),
 	Methods: []grpc.MethodDesc{
