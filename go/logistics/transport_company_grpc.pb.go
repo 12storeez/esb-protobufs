c0297e8a05bfbdc8ad917fee4a06587285102263
d.zotov <d.zotov@12storeez.com>
2021-07-15 17:00:29 +0300

    Generate new pb client and server

--- go/logistics/transport_company_grpc.pb.go
+++ go/logistics/transport_company_grpc.pb.go
@@ -12,6 +12,7 @@ import (
 
 // This is a compile-time assertion to ensure that this generated file
 // is compatible with the grpc package it is being compiled against.
+// Requires gRPC-Go v1.32.0 or later.
 const _ = grpc.SupportPackageIsVersion7
 
 // TransportCompaniesClient is the client API for TransportCompanies service.
@@ -119,7 +120,7 @@ type UnsafeTransportCompaniesServer interface {
 }
 
 func RegisterTransportCompaniesServer(s grpc.ServiceRegistrar, srv TransportCompaniesServer) {
-	s.RegisterService(&_TransportCompanies_serviceDesc, srv)
+	s.RegisterService(&TransportCompanies_ServiceDesc, srv)
 }
 
 func _TransportCompanies_Create_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
@@ -212,7 +213,10 @@ func _TransportCompanies_Delete_Handler(srv interface{}, ctx context.Context, de
 	return interceptor(ctx, in, info, handler)
 }
 
-var _TransportCompanies_serviceDesc = grpc.ServiceDesc{
+// TransportCompanies_ServiceDesc is the grpc.ServiceDesc for TransportCompanies service.
+// It's only intended for direct use with grpc.RegisterService,
+// and not to be introspected or modified (even as a copy)
+var TransportCompanies_ServiceDesc = grpc.ServiceDesc{
 	ServiceName: "logistics.TransportCompanies",
 	HandlerType: (*TransportCompaniesServer)(nil),
 	Methods: []grpc.MethodDesc{
