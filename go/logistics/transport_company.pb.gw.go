30152ca13621095472792d73fe99e9fb9d3c2bd9
d.zotov <d.zotov@12storeez.com>
2021-12-17 16:43:11 +0300

    Transport_company update protobufs

--- go/logistics/transport_company.pb.gw.go
+++ go/logistics/transport_company.pb.gw.go
@@ -539,15 +539,15 @@ func RegisterTransportCompaniesHandlerClient(ctx context.Context, mux *runtime.S
 }
 
 var (
-	pattern_TransportCompanies_List_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1}, []string{"v1", "transport-company"}, ""))
+	pattern_TransportCompanies_List_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2}, []string{"api", "v1", "transport-company"}, ""))
 
-	pattern_TransportCompanies_Create_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1}, []string{"v1", "transport-company"}, ""))
+	pattern_TransportCompanies_Create_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2}, []string{"api", "v1", "transport-company"}, ""))
 
-	pattern_TransportCompanies_Get_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 1, 0, 4, 1, 5, 2}, []string{"v1", "transport-company", "id"}, ""))
+	pattern_TransportCompanies_Get_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2, 1, 0, 4, 1, 5, 3}, []string{"api", "v1", "transport-company", "id"}, ""))
 
-	pattern_TransportCompanies_Update_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 1, 0, 4, 1, 5, 2}, []string{"v1", "transport-company", "id"}, ""))
+	pattern_TransportCompanies_Update_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2, 1, 0, 4, 1, 5, 3}, []string{"api", "v1", "transport-company", "id"}, ""))
 
-	pattern_TransportCompanies_Delete_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 1, 0, 4, 1, 5, 2}, []string{"v1", "transport-company", "id"}, ""))
+	pattern_TransportCompanies_Delete_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2, 1, 0, 4, 1, 5, 3}, []string{"api", "v1", "transport-company", "id"}, ""))
 )
 
 var (
