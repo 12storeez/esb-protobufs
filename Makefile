559443421a5758cec4a0cefd5eb5e85c822dcdd5
a.pchelkina <a.pchelkina@12storeez.com>
2022-06-23 12:29:54 +0300

    ITSOL-2142 protobuf-generator improvement

--- Makefile
+++ Makefile
@@ -1,8 +1,10 @@
 .PHONY: push test go
-PWD = /Users/.../go/src/esb-protobufs
+# Тут необходимо указать путь до репозитория esb-protobufs
+PWD = ~/go/src/esb-protobufs
 
-# Если в директории swagger есть папка соответствующая папке протобафа, то файл swagger сгенерируется, если нет, то нет
-# Например, чтобы сгенерировать файл для сервиса нотификаций, нужно создать директорию  swagger/notifications
+# Если в директории swagger есть папка соответствующая папке протобафа, то файл swagger сгенерируется, если нет, то нет.
+# Например, чтобы сгенерировать файл для сервиса нотификаций, нужно создать директорию  swagger/notifications.
+# Иначе будут ошибки вида: "./swagger/*: No such file or directory". Если swagger не нужен, то не обращаем внимание.
 
 # Если в .proto файле (хотя бы одном, в случае с директорией) есть текст "annotations.proto", то
 # используем для всех протобафов директории скрипт "Генерация клиента и сервера с пакетом annotations.proto"
@@ -13,15 +15,18 @@ PWD = /Users/.../go/src/esb-protobufs
 
 # Скрипты для protoc указаны в Read.me
 
+# Запушить новую версию protobuf-generator в yandex registry
 push:
-	bash cat login.json | docker login --username json_key --password-stdin cr.yandex
+	cat login.json | docker login --username json_key --password-stdin cr.yandex
 	docker build --rm=true -t cr.yandex/crpio465qcfelrmo3fv8/esb-protobufs/protobuf-generator .
 	docker push cr.yandex/crpio465qcfelrmo3fv8/esb-protobufs/protobuf-generator
 
+# Собрать и запустить контейнер локально
 test:
 	docker build --rm=true -f Dockerfile -t protobuf-generator .
 	docker run --rm -v $(PWD):/app -w /app protobuf-generator
 
+# Скачать и запустить последнюю версию protobuf-generator из yandex registry
 go:
 	docker run --rm -v $(PWD):/app -w /app cr.yandex/crpio465qcfelrmo3fv8/esb-protobufs/protobuf-generator
 
--- protoc-gen.sh
+++ protoc-gen.sh
@@ -6,11 +6,11 @@ do
     dirname=$(basename "$file")
 
     ### Генерация единого сваггера для нескольких прото файлов
-    protoc -I $file -I ../tmp/googleapis --openapiv2_out=allow_merge=true,merge_file_name=$dirname.json:./swagger/$dirname $file/*.proto
+    protoc -I $file -I ../git_repo/googleapis --openapiv2_out=allow_merge=true,merge_file_name=$dirname.json:./swagger/$dirname $file/*.proto
 
     if grep -r -q "annotations.proto" $file; then
       ### Генерация клиента и сервера с пакетом annotations.proto:
-      ls $file/ | xargs -I {} protoc -I $file -I ../tmp/googleapis --go_out=. --go-grpc_out=require_unimplemented_servers=false:. $file/{}
+      ls $file/ | xargs -I {} protoc -I $file -I ../git_repo/googleapis --go_out=. --go-grpc_out=require_unimplemented_servers=false:. $file/{}
     else
       ### Генерация прото:
       ls $file/ | xargs -I {} protoc -I . --go_out=. --go-grpc_out=require_unimplemented_servers=false:. $file/{}
@@ -18,12 +18,12 @@ do
 
     if grep -r -q "google.api.http" $file; then
       ### Генерация прокси:
-      ls $file/ |grep "google.api.http"| xargs -I {} protoc -I $file --grpc-gateway_out=. -I ../tmp/googleapis --grpc-gateway_opt logtostderr=true --grpc-gateway_opt generate_unbound_methods=true $file/{}
+      ls $file/ |grep "google.api.http"| xargs -I {} protoc -I $file --grpc-gateway_out=. -I ../git_repo/googleapis --grpc-gateway_opt logtostderr=true --grpc-gateway_opt generate_unbound_methods=true $file/{}
     fi
   elif [ -f "$file" ]; then
     if grep -q "annotations.proto" $file; then
-      #Генерация клиента и сервера с пакетом annotations.proto:
-      protoc -I . -I ../tmp/googleapis --go_out=. --go-grpc_out=require_unimplemented_servers=false:. $file
+      ### Генерация клиента и сервера с пакетом annotations.proto:
+      protoc -I . -I ../git_repo/googleapis --go_out=. --go-grpc_out=require_unimplemented_servers=false:. $file
     else
       ### Генерация прото:
       protoc -I . --go_out=. --go-grpc_out=require_unimplemented_servers=false:. $file
@@ -31,7 +31,7 @@ do
 
     if grep -q "google.api.http" $file; then
       #Генерация прокси:
-      protoc -I . --grpc-gateway_out=. -I ../tmp/googleapis --grpc-gateway_opt logtostderr=true --grpc-gateway_opt generate_unbound_methods=true $file
+      protoc -I . --grpc-gateway_out=. -I ../git_repo/googleapis --grpc-gateway_opt logtostderr=true --grpc-gateway_opt generate_unbound_methods=true $file
     fi
   fi
 done
