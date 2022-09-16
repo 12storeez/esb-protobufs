b6cd5f13ae54bd047c660b9bf168659394d3c208
a.pchelkina <a.pchelkina@12storeez.com>
2022-09-16 13:37:27 +0300

    LGS-49 ZoneGroupRestriction put

--- go/logistics/zone_group_restriction.pb.gw.go
+++ go/logistics/zone_group_restriction.pb.gw.go
@@ -67,41 +67,7 @@ func local_request_ZoneGroupRestrictionService_List_0(ctx context.Context, marsh
 
 }
 
-func request_ZoneGroupRestrictionService_Create_0(ctx context.Context, marshaler runtime.Marshaler, client ZoneGroupRestrictionServiceClient, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
-	var protoReq CreateListZoneGroupRestrictionRequest
-	var metadata runtime.ServerMetadata
-
-	newReader, berr := utilities.IOReaderFactory(req.Body)
-	if berr != nil {
-		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", berr)
-	}
-	if err := marshaler.NewDecoder(newReader()).Decode(&protoReq); err != nil && err != io.EOF {
-		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
-	}
-
-	msg, err := client.Create(ctx, &protoReq, grpc.Header(&metadata.HeaderMD), grpc.Trailer(&metadata.TrailerMD))
-	return msg, metadata, err
-
-}
-
-func local_request_ZoneGroupRestrictionService_Create_0(ctx context.Context, marshaler runtime.Marshaler, server ZoneGroupRestrictionServiceServer, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
-	var protoReq CreateListZoneGroupRestrictionRequest
-	var metadata runtime.ServerMetadata
-
-	newReader, berr := utilities.IOReaderFactory(req.Body)
-	if berr != nil {
-		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", berr)
-	}
-	if err := marshaler.NewDecoder(newReader()).Decode(&protoReq); err != nil && err != io.EOF {
-		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
-	}
-
-	msg, err := server.Create(ctx, &protoReq)
-	return msg, metadata, err
-
-}
-
-func request_ZoneGroupRestrictionService_Update_0(ctx context.Context, marshaler runtime.Marshaler, client ZoneGroupRestrictionServiceClient, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
+func request_ZoneGroupRestrictionService_Upsert_0(ctx context.Context, marshaler runtime.Marshaler, client ZoneGroupRestrictionServiceClient, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
 	var protoReq UpdateListZoneGroupRestrictionRequest
 	var metadata runtime.ServerMetadata
 
@@ -113,12 +79,12 @@ func request_ZoneGroupRestrictionService_Update_0(ctx context.Context, marshaler
 		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
 	}
 
-	msg, err := client.Update(ctx, &protoReq, grpc.Header(&metadata.HeaderMD), grpc.Trailer(&metadata.TrailerMD))
+	msg, err := client.Upsert(ctx, &protoReq, grpc.Header(&metadata.HeaderMD), grpc.Trailer(&metadata.TrailerMD))
 	return msg, metadata, err
 
 }
 
-func local_request_ZoneGroupRestrictionService_Update_0(ctx context.Context, marshaler runtime.Marshaler, server ZoneGroupRestrictionServiceServer, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
+func local_request_ZoneGroupRestrictionService_Upsert_0(ctx context.Context, marshaler runtime.Marshaler, server ZoneGroupRestrictionServiceServer, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
 	var protoReq UpdateListZoneGroupRestrictionRequest
 	var metadata runtime.ServerMetadata
 
@@ -130,7 +96,7 @@ func local_request_ZoneGroupRestrictionService_Update_0(ctx context.Context, mar
 		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
 	}
 
-	msg, err := server.Update(ctx, &protoReq)
+	msg, err := server.Upsert(ctx, &protoReq)
 	return msg, metadata, err
 
 }
@@ -270,7 +236,7 @@ func RegisterZoneGroupRestrictionServiceHandlerServer(ctx context.Context, mux *
 
 	})
 
-	mux.Handle("POST", pattern_ZoneGroupRestrictionService_Create_0, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
+	mux.Handle("PUT", pattern_ZoneGroupRestrictionService_Upsert_0, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
 		ctx, cancel := context.WithCancel(req.Context())
 		defer cancel()
 		var stream runtime.ServerTransportStream
@@ -278,12 +244,12 @@ func RegisterZoneGroupRestrictionServiceHandlerServer(ctx context.Context, mux *
 		inboundMarshaler, outboundMarshaler := runtime.MarshalerForRequest(mux, req)
 		var err error
 		var annotatedContext context.Context
-		annotatedContext, err = runtime.AnnotateIncomingContext(ctx, mux, req, "/logistics.ZoneGroupRestrictionService/Create", runtime.WithHTTPPathPattern("/api/v1/restriction/zone"))
+		annotatedContext, err = runtime.AnnotateIncomingContext(ctx, mux, req, "/logistics.ZoneGroupRestrictionService/Upsert", runtime.WithHTTPPathPattern("/api/v1/restriction/zone"))
 		if err != nil {
 			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
 			return
 		}
-		resp, md, err := local_request_ZoneGroupRestrictionService_Create_0(annotatedContext, inboundMarshaler, server, req, pathParams)
+		resp, md, err := local_request_ZoneGroupRestrictionService_Upsert_0(annotatedContext, inboundMarshaler, server, req, pathParams)
 		md.HeaderMD, md.TrailerMD = metadata.Join(md.HeaderMD, stream.Header()), metadata.Join(md.TrailerMD, stream.Trailer())
 		annotatedContext = runtime.NewServerMetadataContext(annotatedContext, md)
 		if err != nil {
@@ -291,32 +257,7 @@ func RegisterZoneGroupRestrictionServiceHandlerServer(ctx context.Context, mux *
 			return
 		}
 
-		forward_ZoneGroupRestrictionService_Create_0(annotatedContext, mux, outboundMarshaler, w, req, resp, mux.GetForwardResponseOptions()...)
-
-	})
-
-	mux.Handle("PATCH", pattern_ZoneGroupRestrictionService_Update_0, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
-		ctx, cancel := context.WithCancel(req.Context())
-		defer cancel()
-		var stream runtime.ServerTransportStream
-		ctx = grpc.NewContextWithServerTransportStream(ctx, &stream)
-		inboundMarshaler, outboundMarshaler := runtime.MarshalerForRequest(mux, req)
-		var err error
-		var annotatedContext context.Context
-		annotatedContext, err = runtime.AnnotateIncomingContext(ctx, mux, req, "/logistics.ZoneGroupRestrictionService/Update", runtime.WithHTTPPathPattern("/api/v1/restriction/zone"))
-		if err != nil {
-			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
-			return
-		}
-		resp, md, err := local_request_ZoneGroupRestrictionService_Update_0(annotatedContext, inboundMarshaler, server, req, pathParams)
-		md.HeaderMD, md.TrailerMD = metadata.Join(md.HeaderMD, stream.Header()), metadata.Join(md.TrailerMD, stream.Trailer())
-		annotatedContext = runtime.NewServerMetadataContext(annotatedContext, md)
-		if err != nil {
-			runtime.HTTPError(annotatedContext, mux, outboundMarshaler, w, req, err)
-			return
-		}
-
-		forward_ZoneGroupRestrictionService_Update_0(annotatedContext, mux, outboundMarshaler, w, req, resp, mux.GetForwardResponseOptions()...)
+		forward_ZoneGroupRestrictionService_Upsert_0(annotatedContext, mux, outboundMarshaler, w, req, resp, mux.GetForwardResponseOptions()...)
 
 	})
 
@@ -433,47 +374,25 @@ func RegisterZoneGroupRestrictionServiceHandlerClient(ctx context.Context, mux *
 
 	})
 
-	mux.Handle("POST", pattern_ZoneGroupRestrictionService_Create_0, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
+	mux.Handle("PUT", pattern_ZoneGroupRestrictionService_Upsert_0, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
 		ctx, cancel := context.WithCancel(req.Context())
 		defer cancel()
 		inboundMarshaler, outboundMarshaler := runtime.MarshalerForRequest(mux, req)
 		var err error
 		var annotatedContext context.Context
-		annotatedContext, err = runtime.AnnotateContext(ctx, mux, req, "/logistics.ZoneGroupRestrictionService/Create", runtime.WithHTTPPathPattern("/api/v1/restriction/zone"))
+		annotatedContext, err = runtime.AnnotateContext(ctx, mux, req, "/logistics.ZoneGroupRestrictionService/Upsert", runtime.WithHTTPPathPattern("/api/v1/restriction/zone"))
 		if err != nil {
 			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
 			return
 		}
-		resp, md, err := request_ZoneGroupRestrictionService_Create_0(annotatedContext, inboundMarshaler, client, req, pathParams)
+		resp, md, err := request_ZoneGroupRestrictionService_Upsert_0(annotatedContext, inboundMarshaler, client, req, pathParams)
 		annotatedContext = runtime.NewServerMetadataContext(annotatedContext, md)
 		if err != nil {
 			runtime.HTTPError(annotatedContext, mux, outboundMarshaler, w, req, err)
 			return
 		}
 
-		forward_ZoneGroupRestrictionService_Create_0(annotatedContext, mux, outboundMarshaler, w, req, resp, mux.GetForwardResponseOptions()...)
-
-	})
-
-	mux.Handle("PATCH", pattern_ZoneGroupRestrictionService_Update_0, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
-		ctx, cancel := context.WithCancel(req.Context())
-		defer cancel()
-		inboundMarshaler, outboundMarshaler := runtime.MarshalerForRequest(mux, req)
-		var err error
-		var annotatedContext context.Context
-		annotatedContext, err = runtime.AnnotateContext(ctx, mux, req, "/logistics.ZoneGroupRestrictionService/Update", runtime.WithHTTPPathPattern("/api/v1/restriction/zone"))
-		if err != nil {
-			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
-			return
-		}
-		resp, md, err := request_ZoneGroupRestrictionService_Update_0(annotatedContext, inboundMarshaler, client, req, pathParams)
-		annotatedContext = runtime.NewServerMetadataContext(annotatedContext, md)
-		if err != nil {
-			runtime.HTTPError(annotatedContext, mux, outboundMarshaler, w, req, err)
-			return
-		}
-
-		forward_ZoneGroupRestrictionService_Update_0(annotatedContext, mux, outboundMarshaler, w, req, resp, mux.GetForwardResponseOptions()...)
+		forward_ZoneGroupRestrictionService_Upsert_0(annotatedContext, mux, outboundMarshaler, w, req, resp, mux.GetForwardResponseOptions()...)
 
 	})
 
@@ -527,9 +446,7 @@ func RegisterZoneGroupRestrictionServiceHandlerClient(ctx context.Context, mux *
 var (
 	pattern_ZoneGroupRestrictionService_List_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2, 2, 3}, []string{"api", "v1", "restriction", "zone"}, ""))
 
-	pattern_ZoneGroupRestrictionService_Create_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2, 2, 3}, []string{"api", "v1", "restriction", "zone"}, ""))
-
-	pattern_ZoneGroupRestrictionService_Update_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2, 2, 3}, []string{"api", "v1", "restriction", "zone"}, ""))
+	pattern_ZoneGroupRestrictionService_Upsert_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2, 2, 3}, []string{"api", "v1", "restriction", "zone"}, ""))
 
 	pattern_ZoneGroupRestrictionService_Get_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2, 2, 3, 1, 0, 4, 1, 5, 4}, []string{"api", "v1", "restriction", "zone", "id"}, ""))
 
@@ -539,9 +456,7 @@ var (
 var (
 	forward_ZoneGroupRestrictionService_List_0 = runtime.ForwardResponseMessage
 
-	forward_ZoneGroupRestrictionService_Create_0 = runtime.ForwardResponseMessage
-
-	forward_ZoneGroupRestrictionService_Update_0 = runtime.ForwardResponseMessage
+	forward_ZoneGroupRestrictionService_Upsert_0 = runtime.ForwardResponseMessage
 
 	forward_ZoneGroupRestrictionService_Get_0 = runtime.ForwardResponseMessage
 
