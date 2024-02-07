91bb41418aaf642f251b4bebd467cc1b1d133f0f
v.tolmachev <v.tolmachev@12storeez.com>
2024-02-07 05:26:05 +0300

    feat: refactor proto message

--- go/logistics/common.pb.go
+++ go/logistics/common.pb.go
@@ -26,10 +26,12 @@ type UnifiedResponse struct {
 	sizeCache     protoimpl.SizeCache
 	unknownFields protoimpl.UnknownFields
 
-	// Types that are assignable to Response:
-	//	*UnifiedResponse_Fail
-	//	*UnifiedResponse_Success
-	Response isUnifiedResponse_Response `protobuf_oneof:"response"`
+	Status     string     `protobuf:"bytes,1,opt,name=status,proto3" json:"status,omitempty"`
+	Message    string     `protobuf:"bytes,2,opt,name=message,proto3" json:"message,omitempty"`
+	Pagination *Paginate  `protobuf:"bytes,3,opt,name=pagination,proto3,oneof" json:"pagination,omitempty"`
+	Data       *anypb.Any `protobuf:"bytes,4,opt,name=data,proto3,oneof" json:"data,omitempty"`
+	Code       *int32     `protobuf:"zigzag32,5,opt,name=code,proto3,oneof" json:"code,omitempty"`
+	Errors     []*Error   `protobuf:"bytes,6,rep,name=errors,proto3" json:"errors,omitempty"`
 }
 
 func (x *UnifiedResponse) Reset() {
@@ -64,181 +66,44 @@ func (*UnifiedResponse) Descriptor() ([]byte, []int) {
 	return file_common_proto_rawDescGZIP(), []int{0}
 }
 
-func (m *UnifiedResponse) GetResponse() isUnifiedResponse_Response {
-	if m != nil {
-		return m.Response
-	}
-	return nil
-}
-
-func (x *UnifiedResponse) GetFail() *FailResponse {
-	if x, ok := x.GetResponse().(*UnifiedResponse_Fail); ok {
-		return x.Fail
-	}
-	return nil
-}
-
-func (x *UnifiedResponse) GetSuccess() *SuccessRespnse {
-	if x, ok := x.GetResponse().(*UnifiedResponse_Success); ok {
-		return x.Success
-	}
-	return nil
-}
-
-type isUnifiedResponse_Response interface {
-	isUnifiedResponse_Response()
-}
-
-type UnifiedResponse_Fail struct {
-	Fail *FailResponse `protobuf:"bytes,1,opt,name=fail,proto3,oneof"`
-}
-
-type UnifiedResponse_Success struct {
-	Success *SuccessRespnse `protobuf:"bytes,2,opt,name=success,proto3,oneof"`
-}
-
-func (*UnifiedResponse_Fail) isUnifiedResponse_Response() {}
-
-func (*UnifiedResponse_Success) isUnifiedResponse_Response() {}
-
-type FailResponse struct {
-	state         protoimpl.MessageState
-	sizeCache     protoimpl.SizeCache
-	unknownFields protoimpl.UnknownFields
-
-	Status  string   `protobuf:"bytes,1,opt,name=status,proto3" json:"status,omitempty"`
-	Message string   `protobuf:"bytes,2,opt,name=message,proto3" json:"message,omitempty"`
-	Code    int32    `protobuf:"zigzag32,3,opt,name=code,proto3" json:"code,omitempty"`
-	Errors  []*Error `protobuf:"bytes,4,rep,name=errors,proto3" json:"errors,omitempty"`
-}
-
-func (x *FailResponse) Reset() {
-	*x = FailResponse{}
-	if protoimpl.UnsafeEnabled {
-		mi := &file_common_proto_msgTypes[1]
-		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
-		ms.StoreMessageInfo(mi)
-	}
-}
-
-func (x *FailResponse) String() string {
-	return protoimpl.X.MessageStringOf(x)
-}
-
-func (*FailResponse) ProtoMessage() {}
-
-func (x *FailResponse) ProtoReflect() protoreflect.Message {
-	mi := &file_common_proto_msgTypes[1]
-	if protoimpl.UnsafeEnabled && x != nil {
-		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
-		if ms.LoadMessageInfo() == nil {
-			ms.StoreMessageInfo(mi)
-		}
-		return ms
-	}
-	return mi.MessageOf(x)
-}
-
-// Deprecated: Use FailResponse.ProtoReflect.Descriptor instead.
-func (*FailResponse) Descriptor() ([]byte, []int) {
-	return file_common_proto_rawDescGZIP(), []int{1}
-}
-
-func (x *FailResponse) GetStatus() string {
+func (x *UnifiedResponse) GetStatus() string {
 	if x != nil {
 		return x.Status
 	}
 	return ""
 }
 
-func (x *FailResponse) GetMessage() string {
+func (x *UnifiedResponse) GetMessage() string {
 	if x != nil {
 		return x.Message
 	}
 	return ""
 }
 
-func (x *FailResponse) GetCode() int32 {
-	if x != nil {
-		return x.Code
-	}
-	return 0
-}
-
-func (x *FailResponse) GetErrors() []*Error {
-	if x != nil {
-		return x.Errors
-	}
-	return nil
-}
-
-type SuccessRespnse struct {
-	state         protoimpl.MessageState
-	sizeCache     protoimpl.SizeCache
-	unknownFields protoimpl.UnknownFields
-
-	Status     string     `protobuf:"bytes,1,opt,name=status,proto3" json:"status,omitempty"`
-	Message    string     `protobuf:"bytes,2,opt,name=message,proto3" json:"message,omitempty"`
-	Pagination *Paginate  `protobuf:"bytes,3,opt,name=pagination,proto3" json:"pagination,omitempty"`
-	Data       *anypb.Any `protobuf:"bytes,4,opt,name=data,proto3" json:"data,omitempty"`
-}
-
-func (x *SuccessRespnse) Reset() {
-	*x = SuccessRespnse{}
-	if protoimpl.UnsafeEnabled {
-		mi := &file_common_proto_msgTypes[2]
-		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
-		ms.StoreMessageInfo(mi)
-	}
-}
-
-func (x *SuccessRespnse) String() string {
-	return protoimpl.X.MessageStringOf(x)
-}
-
-func (*SuccessRespnse) ProtoMessage() {}
-
-func (x *SuccessRespnse) ProtoReflect() protoreflect.Message {
-	mi := &file_common_proto_msgTypes[2]
-	if protoimpl.UnsafeEnabled && x != nil {
-		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
-		if ms.LoadMessageInfo() == nil {
-			ms.StoreMessageInfo(mi)
-		}
-		return ms
-	}
-	return mi.MessageOf(x)
-}
-
-// Deprecated: Use SuccessRespnse.ProtoReflect.Descriptor instead.
-func (*SuccessRespnse) Descriptor() ([]byte, []int) {
-	return file_common_proto_rawDescGZIP(), []int{2}
-}
-
-func (x *SuccessRespnse) GetStatus() string {
-	if x != nil {
-		return x.Status
-	}
-	return ""
-}
-
-func (x *SuccessRespnse) GetMessage() string {
-	if x != nil {
-		return x.Message
-	}
-	return ""
-}
-
-func (x *SuccessRespnse) GetPagination() *Paginate {
+func (x *UnifiedResponse) GetPagination() *Paginate {
 	if x != nil {
 		return x.Pagination
 	}
 	return nil
 }
 
-func (x *SuccessRespnse) GetData() *anypb.Any {
+func (x *UnifiedResponse) GetData() *anypb.Any {
 	if x != nil {
 		return x.Data
+	}
+	return nil
+}
+
+func (x *UnifiedResponse) GetCode() int32 {
+	if x != nil && x.Code != nil {
+		return *x.Code
+	}
+	return 0
+}
+
+func (x *UnifiedResponse) GetErrors() []*Error {
+	if x != nil {
+		return x.Errors
 	}
 	return nil
 }
@@ -256,7 +121,7 @@ type Error struct {
 func (x *Error) Reset() {
 	*x = Error{}
 	if protoimpl.UnsafeEnabled {
-		mi := &file_common_proto_msgTypes[3]
+		mi := &file_common_proto_msgTypes[1]
 		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
 		ms.StoreMessageInfo(mi)
 	}
@@ -269,7 +134,7 @@ func (x *Error) String() string {
 func (*Error) ProtoMessage() {}
 
 func (x *Error) ProtoReflect() protoreflect.Message {
-	mi := &file_common_proto_msgTypes[3]
+	mi := &file_common_proto_msgTypes[1]
 	if protoimpl.UnsafeEnabled && x != nil {
 		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
 		if ms.LoadMessageInfo() == nil {
@@ -282,7 +147,7 @@ func (x *Error) ProtoReflect() protoreflect.Message {
 
 // Deprecated: Use Error.ProtoReflect.Descriptor instead.
 func (*Error) Descriptor() ([]byte, []int) {
-	return file_common_proto_rawDescGZIP(), []int{3}
+	return file_common_proto_rawDescGZIP(), []int{1}
 }
 
 func (x *Error) GetTarget() string {
@@ -319,7 +184,7 @@ type Paginate struct {
 func (x *Paginate) Reset() {
 	*x = Paginate{}
 	if protoimpl.UnsafeEnabled {
-		mi := &file_common_proto_msgTypes[4]
+		mi := &file_common_proto_msgTypes[2]
 		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
 		ms.StoreMessageInfo(mi)
 	}
@@ -332,7 +197,7 @@ func (x *Paginate) String() string {
 func (*Paginate) ProtoMessage() {}
 
 func (x *Paginate) ProtoReflect() protoreflect.Message {
-	mi := &file_common_proto_msgTypes[4]
+	mi := &file_common_proto_msgTypes[2]
 	if protoimpl.UnsafeEnabled && x != nil {
 		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
 		if ms.LoadMessageInfo() == nil {
@@ -345,7 +210,7 @@ func (x *Paginate) ProtoReflect() protoreflect.Message {
 
 // Deprecated: Use Paginate.ProtoReflect.Descriptor instead.
 func (*Paginate) Descriptor() ([]byte, []int) {
-	return file_common_proto_rawDescGZIP(), []int{4}
+	return file_common_proto_rawDescGZIP(), []int{2}
 }
 
 func (x *Paginate) GetLimit() uint32 {
@@ -375,32 +240,23 @@ var file_common_proto_rawDesc = []byte{
 	0x0a, 0x0c, 0x63, 0x6f, 0x6d, 0x6d, 0x6f, 0x6e, 0x2e, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x12, 0x06,
 	0x63, 0x6f, 0x6d, 0x6d, 0x6f, 0x6e, 0x1a, 0x19, 0x67, 0x6f, 0x6f, 0x67, 0x6c, 0x65, 0x2f, 0x70,
 	0x72, 0x6f, 0x74, 0x6f, 0x62, 0x75, 0x66, 0x2f, 0x61, 0x6e, 0x79, 0x2e, 0x70, 0x72, 0x6f, 0x74,
-	0x6f, 0x22, 0x7d, 0x0a, 0x0f, 0x55, 0x6e, 0x69, 0x66, 0x69, 0x65, 0x64, 0x52, 0x65, 0x73, 0x70,
-	0x6f, 0x6e, 0x73, 0x65, 0x12, 0x2a, 0x0a, 0x04, 0x66, 0x61, 0x69, 0x6c, 0x18, 0x01, 0x20, 0x01,
-	0x28, 0x0b, 0x32, 0x14, 0x2e, 0x63, 0x6f, 0x6d, 0x6d, 0x6f, 0x6e, 0x2e, 0x46, 0x61, 0x69, 0x6c,
-	0x52, 0x65, 0x73, 0x70, 0x6f, 0x6e, 0x73, 0x65, 0x48, 0x00, 0x52, 0x04, 0x66, 0x61, 0x69, 0x6c,
-	0x12, 0x32, 0x0a, 0x07, 0x73, 0x75, 0x63, 0x63, 0x65, 0x73, 0x73, 0x18, 0x02, 0x20, 0x01, 0x28,
-	0x0b, 0x32, 0x16, 0x2e, 0x63, 0x6f, 0x6d, 0x6d, 0x6f, 0x6e, 0x2e, 0x53, 0x75, 0x63, 0x63, 0x65,
-	0x73, 0x73, 0x52, 0x65, 0x73, 0x70, 0x6e, 0x73, 0x65, 0x48, 0x00, 0x52, 0x07, 0x73, 0x75, 0x63,
-	0x63, 0x65, 0x73, 0x73, 0x42, 0x0a, 0x0a, 0x08, 0x72, 0x65, 0x73, 0x70, 0x6f, 0x6e, 0x73, 0x65,
-	0x22, 0x7b, 0x0a, 0x0c, 0x46, 0x61, 0x69, 0x6c, 0x52, 0x65, 0x73, 0x70, 0x6f, 0x6e, 0x73, 0x65,
-	0x12, 0x16, 0x0a, 0x06, 0x73, 0x74, 0x61, 0x74, 0x75, 0x73, 0x18, 0x01, 0x20, 0x01, 0x28, 0x09,
-	0x52, 0x06, 0x73, 0x74, 0x61, 0x74, 0x75, 0x73, 0x12, 0x18, 0x0a, 0x07, 0x6d, 0x65, 0x73, 0x73,
-	0x61, 0x67, 0x65, 0x18, 0x02, 0x20, 0x01, 0x28, 0x09, 0x52, 0x07, 0x6d, 0x65, 0x73, 0x73, 0x61,
-	0x67, 0x65, 0x12, 0x12, 0x0a, 0x04, 0x63, 0x6f, 0x64, 0x65, 0x18, 0x03, 0x20, 0x01, 0x28, 0x11,
-	0x52, 0x04, 0x63, 0x6f, 0x64, 0x65, 0x12, 0x25, 0x0a, 0x06, 0x65, 0x72, 0x72, 0x6f, 0x72, 0x73,
-	0x18, 0x04, 0x20, 0x03, 0x28, 0x0b, 0x32, 0x0d, 0x2e, 0x63, 0x6f, 0x6d, 0x6d, 0x6f, 0x6e, 0x2e,
-	0x45, 0x72, 0x72, 0x6f, 0x72, 0x52, 0x06, 0x65, 0x72, 0x72, 0x6f, 0x72, 0x73, 0x22, 0x9e, 0x01,
-	0x0a, 0x0e, 0x53, 0x75, 0x63, 0x63, 0x65, 0x73, 0x73, 0x52, 0x65, 0x73, 0x70, 0x6e, 0x73, 0x65,
-	0x12, 0x16, 0x0a, 0x06, 0x73, 0x74, 0x61, 0x74, 0x75, 0x73, 0x18, 0x01, 0x20, 0x01, 0x28, 0x09,
-	0x52, 0x06, 0x73, 0x74, 0x61, 0x74, 0x75, 0x73, 0x12, 0x18, 0x0a, 0x07, 0x6d, 0x65, 0x73, 0x73,
-	0x61, 0x67, 0x65, 0x18, 0x02, 0x20, 0x01, 0x28, 0x09, 0x52, 0x07, 0x6d, 0x65, 0x73, 0x73, 0x61,
-	0x67, 0x65, 0x12, 0x30, 0x0a, 0x0a, 0x70, 0x61, 0x67, 0x69, 0x6e, 0x61, 0x74, 0x69, 0x6f, 0x6e,
-	0x18, 0x03, 0x20, 0x01, 0x28, 0x0b, 0x32, 0x10, 0x2e, 0x63, 0x6f, 0x6d, 0x6d, 0x6f, 0x6e, 0x2e,
-	0x50, 0x61, 0x67, 0x69, 0x6e, 0x61, 0x74, 0x65, 0x52, 0x0a, 0x70, 0x61, 0x67, 0x69, 0x6e, 0x61,
-	0x74, 0x69, 0x6f, 0x6e, 0x12, 0x28, 0x0a, 0x04, 0x64, 0x61, 0x74, 0x61, 0x18, 0x04, 0x20, 0x01,
-	0x28, 0x0b, 0x32, 0x14, 0x2e, 0x67, 0x6f, 0x6f, 0x67, 0x6c, 0x65, 0x2e, 0x70, 0x72, 0x6f, 0x74,
-	0x6f, 0x62, 0x75, 0x66, 0x2e, 0x41, 0x6e, 0x79, 0x52, 0x04, 0x64, 0x61, 0x74, 0x61, 0x22, 0x4d,
+	0x6f, 0x22, 0x8a, 0x02, 0x0a, 0x0f, 0x55, 0x6e, 0x69, 0x66, 0x69, 0x65, 0x64, 0x52, 0x65, 0x73,
+	0x70, 0x6f, 0x6e, 0x73, 0x65, 0x12, 0x16, 0x0a, 0x06, 0x73, 0x74, 0x61, 0x74, 0x75, 0x73, 0x18,
+	0x01, 0x20, 0x01, 0x28, 0x09, 0x52, 0x06, 0x73, 0x74, 0x61, 0x74, 0x75, 0x73, 0x12, 0x18, 0x0a,
+	0x07, 0x6d, 0x65, 0x73, 0x73, 0x61, 0x67, 0x65, 0x18, 0x02, 0x20, 0x01, 0x28, 0x09, 0x52, 0x07,
+	0x6d, 0x65, 0x73, 0x73, 0x61, 0x67, 0x65, 0x12, 0x35, 0x0a, 0x0a, 0x70, 0x61, 0x67, 0x69, 0x6e,
+	0x61, 0x74, 0x69, 0x6f, 0x6e, 0x18, 0x03, 0x20, 0x01, 0x28, 0x0b, 0x32, 0x10, 0x2e, 0x63, 0x6f,
+	0x6d, 0x6d, 0x6f, 0x6e, 0x2e, 0x50, 0x61, 0x67, 0x69, 0x6e, 0x61, 0x74, 0x65, 0x48, 0x00, 0x52,
+	0x0a, 0x70, 0x61, 0x67, 0x69, 0x6e, 0x61, 0x74, 0x69, 0x6f, 0x6e, 0x88, 0x01, 0x01, 0x12, 0x2d,
+	0x0a, 0x04, 0x64, 0x61, 0x74, 0x61, 0x18, 0x04, 0x20, 0x01, 0x28, 0x0b, 0x32, 0x14, 0x2e, 0x67,
+	0x6f, 0x6f, 0x67, 0x6c, 0x65, 0x2e, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x62, 0x75, 0x66, 0x2e, 0x41,
+	0x6e, 0x79, 0x48, 0x01, 0x52, 0x04, 0x64, 0x61, 0x74, 0x61, 0x88, 0x01, 0x01, 0x12, 0x17, 0x0a,
+	0x04, 0x63, 0x6f, 0x64, 0x65, 0x18, 0x05, 0x20, 0x01, 0x28, 0x11, 0x48, 0x02, 0x52, 0x04, 0x63,
+	0x6f, 0x64, 0x65, 0x88, 0x01, 0x01, 0x12, 0x25, 0x0a, 0x06, 0x65, 0x72, 0x72, 0x6f, 0x72, 0x73,
+	0x18, 0x06, 0x20, 0x03, 0x28, 0x0b, 0x32, 0x0d, 0x2e, 0x63, 0x6f, 0x6d, 0x6d, 0x6f, 0x6e, 0x2e,
+	0x45, 0x72, 0x72, 0x6f, 0x72, 0x52, 0x06, 0x65, 0x72, 0x72, 0x6f, 0x72, 0x73, 0x42, 0x0d, 0x0a,
+	0x0b, 0x5f, 0x70, 0x61, 0x67, 0x69, 0x6e, 0x61, 0x74, 0x69, 0x6f, 0x6e, 0x42, 0x07, 0x0a, 0x05,
+	0x5f, 0x64, 0x61, 0x74, 0x61, 0x42, 0x07, 0x0a, 0x05, 0x5f, 0x63, 0x6f, 0x64, 0x65, 0x22, 0x4d,
 	0x0a, 0x05, 0x45, 0x72, 0x72, 0x6f, 0x72, 0x12, 0x16, 0x0a, 0x06, 0x74, 0x61, 0x72, 0x67, 0x65,
 	0x74, 0x18, 0x01, 0x20, 0x01, 0x28, 0x09, 0x52, 0x06, 0x74, 0x61, 0x72, 0x67, 0x65, 0x74, 0x12,
 	0x12, 0x0a, 0x04, 0x63, 0x6f, 0x64, 0x65, 0x18, 0x02, 0x20, 0x01, 0x28, 0x11, 0x52, 0x04, 0x63,
@@ -427,26 +283,22 @@ func file_common_proto_rawDescGZIP() []byte {
 	return file_common_proto_rawDescData
 }
 
-var file_common_proto_msgTypes = make([]protoimpl.MessageInfo, 5)
+var file_common_proto_msgTypes = make([]protoimpl.MessageInfo, 3)
 var file_common_proto_goTypes = []interface{}{
 	(*UnifiedResponse)(nil), // 0: common.UnifiedResponse
-	(*FailResponse)(nil),    // 1: common.FailResponse
-	(*SuccessRespnse)(nil),  // 2: common.SuccessRespnse
-	(*Error)(nil),           // 3: common.Error
-	(*Paginate)(nil),        // 4: common.Paginate
-	(*anypb.Any)(nil),       // 5: google.protobuf.Any
+	(*Error)(nil),           // 1: common.Error
+	(*Paginate)(nil),        // 2: common.Paginate
+	(*anypb.Any)(nil),       // 3: google.protobuf.Any
 }
 var file_common_proto_depIdxs = []int32{
-	1, // 0: common.UnifiedResponse.fail:type_name -> common.FailResponse
-	2, // 1: common.UnifiedResponse.success:type_name -> common.SuccessRespnse
-	3, // 2: common.FailResponse.errors:type_name -> common.Error
-	4, // 3: common.SuccessRespnse.pagination:type_name -> common.Paginate
-	5, // 4: common.SuccessRespnse.data:type_name -> google.protobuf.Any
-	5, // [5:5] is the sub-list for method output_type
-	5, // [5:5] is the sub-list for method input_type
-	5, // [5:5] is the sub-list for extension type_name
-	5, // [5:5] is the sub-list for extension extendee
-	0, // [0:5] is the sub-list for field type_name
+	2, // 0: common.UnifiedResponse.pagination:type_name -> common.Paginate
+	3, // 1: common.UnifiedResponse.data:type_name -> google.protobuf.Any
+	1, // 2: common.UnifiedResponse.errors:type_name -> common.Error
+	3, // [3:3] is the sub-list for method output_type
+	3, // [3:3] is the sub-list for method input_type
+	3, // [3:3] is the sub-list for extension type_name
+	3, // [3:3] is the sub-list for extension extendee
+	0, // [0:3] is the sub-list for field type_name
 }
 
 func init() { file_common_proto_init() }
@@ -468,30 +320,6 @@ func file_common_proto_init() {
 			}
 		}
 		file_common_proto_msgTypes[1].Exporter = func(v interface{}, i int) interface{} {
-			switch v := v.(*FailResponse); i {
-			case 0:
-				return &v.state
-			case 1:
-				return &v.sizeCache
-			case 2:
-				return &v.unknownFields
-			default:
-				return nil
-			}
-		}
-		file_common_proto_msgTypes[2].Exporter = func(v interface{}, i int) interface{} {
-			switch v := v.(*SuccessRespnse); i {
-			case 0:
-				return &v.state
-			case 1:
-				return &v.sizeCache
-			case 2:
-				return &v.unknownFields
-			default:
-				return nil
-			}
-		}
-		file_common_proto_msgTypes[3].Exporter = func(v interface{}, i int) interface{} {
 			switch v := v.(*Error); i {
 			case 0:
 				return &v.state
@@ -503,7 +331,7 @@ func file_common_proto_init() {
 				return nil
 			}
 		}
-		file_common_proto_msgTypes[4].Exporter = func(v interface{}, i int) interface{} {
+		file_common_proto_msgTypes[2].Exporter = func(v interface{}, i int) interface{} {
 			switch v := v.(*Paginate); i {
 			case 0:
 				return &v.state
@@ -516,17 +344,14 @@ func file_common_proto_init() {
 			}
 		}
 	}
-	file_common_proto_msgTypes[0].OneofWrappers = []interface{}{
-		(*UnifiedResponse_Fail)(nil),
-		(*UnifiedResponse_Success)(nil),
-	}
+	file_common_proto_msgTypes[0].OneofWrappers = []interface{}{}
 	type x struct{}
 	out := protoimpl.TypeBuilder{
 		File: protoimpl.DescBuilder{
 			GoPackagePath: reflect.TypeOf(x{}).PkgPath(),
 			RawDescriptor: file_common_proto_rawDesc,
 			NumEnums:      0,
-			NumMessages:   5,
+			NumMessages:   3,
 			NumExtensions: 0,
 			NumServices:   0,
 		},
